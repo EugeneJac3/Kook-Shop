61cb05f5349e4eb4841117ffa06fec08ae74c72f
Alex Degregori <alexdegregori@gmail.com>
2022-05-15 17:06:57 -0700

    initialize

--- client/src/App.js
+++ client/src/App.js
@@ -7,40 +7,40 @@ import Tutorial from "./components/Tutorial";
 import ProductsList from "./components/ProductsList";
 
 function App() {
-  return (
-    <div>
-      <nav className="navbar navbar-expand navbar-dark bg-dark">
-        <a href="/" className="navbar-brand">
-          Kook Boards
-        </a>
-        <div className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <Link to={"/"} className="nav-link">
-              Surfboards
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/add"} className="nav-link">
-              Shopping Cart
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to={"/add"} className="nav-link">
-              About Us
-            </Link>
-          </li>
-        </div>
-      </nav>
-      <div className="container mt-3">
-        <Routes>
-          <Route path="/" element={<ProductsList />} />
-          {/* <Route path="/tutorials" element={<TutorialsList />} /> */}
-          <Route path="/add" element={<AddTutorial />} />
-          <Route path="/tutorials/:id" element={<Tutorial />} />
-        </Routes>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<nav className="navbar navbar-expand navbar-dark bg-dark">
+				<a href="/" className="navbar-brand">
+					Kook Boards
+				</a>
+				<div className="navbar-nav mr-auto">
+					<li className="nav-item">
+						<Link to={"/"} className="nav-link">
+							Surfboards
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to={"/add"} className="nav-link">
+							Shopping Cart
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to={"/add"} className="nav-link">
+							About Us Page
+						</Link>
+					</li>
+				</div>
+			</nav>
+			<div className="container mt-3">
+				<Routes>
+					<Route path="/" element={<ProductsList />} />
+					{/* <Route path="/tutorials" element={<TutorialsList />} /> */}
+					<Route path="/add" element={<AddTutorial />} />
+					<Route path="/tutorials/:id" element={<Tutorial />} />
+				</Routes>
+			</div>
+		</div>
+	);
 }
 
 export default App;
--- node-express-mongodb/server.js
+++ node-express-mongodb/server.js
@@ -4,7 +4,7 @@ const app = express();
 require("dotenv").config();
 
 var corsOptions = {
-  origin: "http://localhost:8081",
+	origin: "http://localhost:8081",
 };
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
@@ -13,27 +13,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+	res.json({ message: "Welcome to bezkoder application." });
 });
 
 require("./app/routes/tutorial.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+	console.log(`Server is running on port ${PORT}.`);
 });
 
 const db = require("./app/models");
 db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch((err) => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+	.connect(db.url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log("Connected to the database!");
+	})
+	.catch((err) => {
+		console.log("Cannot connect to the database!", err);
+		process.exit();
+	});

228101f98d80b7b742dfb1b6ac34bdd515247b99
kpnguyen9 <kpnguyen2@gmail.com>
2022-05-21 12:29:32 -0500

    added filtering feature

--- client/src/components/ProductsList.js
+++ client/src/components/ProductsList.js
@@ -1,65 +1,58 @@
 import React, { useState, useEffect } from "react";
 import ProductDataService from "../services/ProductService";
-import ProductCards from "./productCards/ProductCards.jsx";
+import ProductCards from "./productCards/productCards.jsx";
 import Grid from "@mui/material/Grid";
 import Checkbox from "@mui/material/Checkbox";
 import Box from "@mui/material/Box";
-import { Typography } from "@mui/material";
+import { ButtonBase, Typography } from "@mui/material";
+import Buttons from "../buttons";
 
 const ProductsList = () => {
-	const [products, setProducts] = useState([]);
-	const [checked, setChecked] = React.useState(true);
+  const [allProducts, setAllProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-	useEffect(() => {
-		retrieveProducts();
-	}, []);
+  const productItems = [...new Set(allProducts.map((Val) => Val.brand))];
+  console.log("initial productItems:", productItems);
 
-	const retrieveProducts = () => {
-		ProductDataService.getAll()
-			.then((response) => {
-				setProducts(response.data);
-				console.log(response.data);
-			})
-			.catch((e) => {
-				console.log(e);
-			});
-	};
+  useEffect(() => {
+    retrieveProducts();
+  }, []);
 
-	const handleChange = (event) => {
-		setChecked(event.target.checked);
-		console.log(checked);
-	};
+  const retrieveProducts = () => {
+    ProductDataService.getAll()
+      .then((response) => {
+        setProducts(response.data);
+        setAllProducts(response.data);
+        // console.log("retrieved products:", response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
 
-	return (
-		<Grid
-			container
-			direction="row"
-			justifyContent="center"
-			alignItems="flex-start"
-		>
-			<Grid item xs={2} className="filter">
-				<h1>Filter Brands</h1>
-				<Box>
-					<Box
-						sx={{
-							display: "flex",
-						}}
-					>
-						<Checkbox
-							checked={checked}
-							onChange={handleChange}
-							inputProps={{ "aria-label": "controlled" }}
-						/>
-						<Typography variant="h5" component="p">
-							Kook
-						</Typography>
-					</Box>
-				</Box>
-			</Grid>
-			<Grid item xs={10}>
-				<ProductCards products={products} key={products._id} />
-			</Grid>
-		</Grid>
-	);
+  return (
+    <Grid
+      container
+      direction="row"
+      //   justifyContent="center"
+      //   alignItems="flex-start"
+    >
+      <Grid item xs={2} className="filter">
+        <Typography variant="h4" component="div" gutterBottom>
+          Filter Brands
+        </Typography>
+        <Box>
+          <Buttons
+            originalProducts={allProducts}
+            setProducts={setProducts}
+            productItems={productItems}
+          />
+        </Box>
+      </Grid>
+      <Grid item xs={10}>
+        <ProductCards products={products} key={products._id} />
+      </Grid>
+    </Grid>
+  );
 };
 export default ProductsList;

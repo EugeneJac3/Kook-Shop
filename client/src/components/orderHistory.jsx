2db72140ebc6faa079970542af55806291b79399
Alex Degregori <alexdegregori@gmail.com>
2022-05-31 17:38:59 -0700

    order history working

--- client/src/components/orderHistory.jsx
+++ client/src/components/orderHistory.jsx
@@ -1,36 +1,38 @@
 import React, { useState, useEffect } from "react";
 import OrderDataService from "../services/ProductService";
-
 import Box from "@mui/material/Box";
 import OrderCards from "./orderCards";
+import jwt_decode from "jwt-decode";
+import { useCookies } from "react-cookie";
 
 const OrderHistory = () => {
-  const [allOrders, setAllOrders] = useState([]);
+	const [allOrders, setAllOrders] = useState([]);
+	const [cookies] = useCookies(["jwt"]);
+	const decoded = jwt_decode(cookies.jwt);
+	const userID = decoded.id;
 
-  useEffect(() => {
-    retrieveOrders();
-  }, []);
+	useEffect(() => {
+		retrieveOrders();
+	}, []);
 
-  const retrieveOrders = () => {
-    OrderDataService.getAllOrders()
-      .then((response) => {
-        console.log("all orders", response.data);
-        const allOrders = response.data;
-        const userOrders = allOrders.filter(
-          (order) => order.userID === "123456"
-        );
+	const retrieveOrders = () => {
+		OrderDataService.getAllOrders()
+			.then((response) => {
+				console.log("all orders", response.data);
+				const allOrders = response.data;
+				const userOrders = allOrders.filter((order) => order.userID === userID);
 
-        setAllOrders(userOrders);
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  };
+				setAllOrders(userOrders);
+			})
+			.catch((e) => {
+				console.log(e);
+			});
+	};
 
-  return (
-    <Box>
-      <OrderCards orders={allOrders} />
-    </Box>
-  );
+	return (
+		<Box>
+			<OrderCards orders={allOrders} />
+		</Box>
+	);
 };
 export default OrderHistory;

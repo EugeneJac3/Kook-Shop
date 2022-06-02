b4dce6c88760e9aa89b74381b4d20590dd36cc59
kpnguyen9 <kpnguyen2@gmail.com>
2022-06-01 20:36:17 -0500

    edited login page

--- client/src/components/users/Login.jsx
+++ client/src/components/users/Login.jsx
@@ -6,90 +6,90 @@ import axios from "axios";
 import { UserContext } from "../../helper/UserContext";
 
 export default function Login() {
-	const [cookies] = useCookies([]);
-	const navigate = useNavigate();
-	const { user, setUser } = useContext(UserContext);
+  const [cookies] = useCookies([]);
+  const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
 
-	useEffect(() => {
-		if (cookies.jwt) {
-			navigate("/");
-		}
-	}, [cookies, navigate]);
+  useEffect(() => {
+    if (cookies.jwt) {
+      navigate("/");
+    }
+  }, [cookies, navigate]);
 
-	const [values, setValues] = useState({
-		email: "",
-		password: "",
-	});
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
 
-	const generateError = (err) =>
-		toast.error(err, {
-			position: "bottom-right",
-		});
+  const generateError = (err) =>
+    toast.error(err, {
+      position: "bottom-right",
+    });
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		try {
-			const { data } = await axios.post(
-				"http://localhost:8080/api/login",
-				{
-					...values,
-				},
-				{
-					withCredentials: true,
-				}
-			);
-			if (data) {
-				if (data.errors) {
-					const { email, password } = data.errors;
-					if (email) generateError(email);
-					else if (password) generateError(password);
-				} else {
-					setUser(data.user);
-					console.log("data.user", data.user);
-					console.log("setUser", user);
-					navigate("/");
-				}
-			}
-		} catch (err) {
-			console.log(err);
-		}
-	};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8080/api/login",
+        {
+          ...values,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      if (data) {
+        if (data.errors) {
+          const { email, password } = data.errors;
+          if (email) generateError(email);
+          else if (password) generateError(password);
+        } else {
+          setUser(data.user);
+          console.log("data.user", data.user);
+          console.log("setUser", user);
+          navigate("/");
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
-	return (
-		<div className="logBody">
-			<div className="Usercontainer">
-				<h2>Login Account</h2>
-				<form onSubmit={(e) => handleSubmit(e)}>
-					<div>
-						<label htmlFor="email">Email</label>
-						<input
-							type="email"
-							name="email"
-							placeholder="Email"
-							onChange={(e) =>
-								setValues({ ...values, [e.target.name]: e.target.value })
-							}
-						/>
-					</div>
-					<div>
-						<label htmlFor="password">Password</label>
-						<input
-							type="password"
-							name="password"
-							placeholder="Password"
-							onChange={(e) =>
-								setValues({ ...values, [e.target.name]: e.target.value })
-							}
-						/>
-					</div>
-					<button className="userButton" type="submit">
-						Submit
-					</button>
-					<span>
-						Already have an account? <Link to="/register">Register</Link>
-					</span>
-				</form>
-			</div>
-		</div>
-	);
+  return (
+    <div className="logBody">
+      <div className="Usercontainer">
+        <h2>Login Account</h2>
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              onChange={(e) =>
+                setValues({ ...values, [e.target.name]: e.target.value })
+              }
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={(e) =>
+                setValues({ ...values, [e.target.name]: e.target.value })
+              }
+            />
+          </div>
+          <button className="userButton" type="submit">
+            Submit
+          </button>
+          <span>
+            Don't have an account? <Link to="/register">Register</Link>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
 }
